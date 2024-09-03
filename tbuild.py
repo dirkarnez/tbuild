ffe1a7ef74f35a54c8e3f355bd33cfbc468d1247
Dirk Arnez <smalldirkalex@gmail.com>
2024-09-04 04:44:22 +0800

    - update tbuild.py

--- tbuild.py
+++ tbuild.py
@@ -44,3 +44,18 @@ class TBuild:
 # gcc -c 
 # gcc -o hello hello.o
 
+
+# https://github.com/dirkarnez/qemu-cortex-a7-baremetal/blob/main/Makefile
+# hello_world.elf: link_script.ld startup.o hello_world.o
+# 	# Link the two object files to an elf file
+# 	$(LD) -T link_script.ld startup.o hello_world.o -o hello_world.elf
+
+# startup.o: startup.s
+# 	$(AS) -o startup.o startup.s
+
+# hello_world.o: hello_world.c
+# 	$(CC) -c -o hello_world.o hello_world.c
+
+# clean:
+# 	rm -f hello_world.o startup.o hello_world.elf
+
