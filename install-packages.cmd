be85cbda8d4b2c78c0b9eb5078db741f5570faa4
Dirk Arnez <smalldirkalex@gmail.com>
2024-09-20 19:46:46 +0800

    - update install-packages.cmd

--- install-packages.cmd
+++ install-packages.cmd
@@ -1,6 +1,14 @@
+@REM run as Administrator
 @echo off
-set PYTHON_DIR=%USERPROFILE%\Downloads\python-3.10.8-amd64-portable
-set PATH=%PYTHON_DIR%;%PYTHON_DIR%\Scripts
+
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+
+set PYTHON_DIR=%DOWNLOADS_DIR%\python-3.10.8-amd64-portable
+
+set PATH=^
+%PYTHON_DIR%;^
+%PYTHON_DIR%\Scripts;^
+%DOWNLOADS_DIR%\PortableGit\bin;
 
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
